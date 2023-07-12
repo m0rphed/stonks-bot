1b22688de6c1f7988cf81a39d71cb5206c1e4270
Vic Khov <victorkhovtko@gmail.com>
2023-07-12 15:49:24 +0300

    simplified credentials functions

--- bot/creds.py
+++ bot/creds.py
@@ -3,51 +3,32 @@ import os
 import toml
 
 
-@dataclass(frozen=True)
-class FromFileTOML:
-    key: str
-    toml: tuple = ("secret", "keys.toml")
+def get_from_toml(key, toml_path: str = "./secret/keys.toml"):
+    # get the file path
+    file_path = os.path.join(toml_path)
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"TOML file '{file_path}' not found.")
+
+    # read keys to memory
+    secrets = toml.load(file_path)
+    if key not in secrets:
+        raise ValueError(
+            f"'{key}' not found in the TOML file.")
+
+    return secrets[key]  # return specified secret key
 
 
-@dataclass(frozen=True)
-class FromEvnVar:
-    key: str
+def get_from_env(key):
+    env_value = os.environ.get(key)
+    if env_value is not None:
+        return env_value
 
-
-ReadOptions = FromFileTOML | FromEvnVar
-
-
-def read_key(read_from: ReadOptions):
-    match read_from:
-        case FromFileTOML(secret_key, toml_path):
-            # get the file path
-            file_path = os.path.join(*toml_path)
-            file_path = os.path.join(os.getcwd(), file_path)
-            if not os.path.exists(file_path):
-                raise FileNotFoundError(f"TOML file '{file_path}' not found.")
-
-            # read keys to memory
-            secrets = toml.load(file_path)
-            if secret_key not in secrets:
-                raise ValueError(
-                    f"'{secret_key}' not found in the TOML file.")
-
-            return secrets[secret_key]  # return specified secret key
-
-        case FromEvnVar(env_name):
-            env_value = os.environ.get(env_name)
-            if env_value is not None:
-                return env_value
-
-            # raise error if ENV VAR not set
-            raise ValueError(f"Environment variable '{env_name}' not set.")
-
-        case _:
-            raise ValueError("Invalid `read_from` parameter.")
+    # raise error if ENV VAR not set
+    raise ValueError(f"Environment variable '{key}' not set.")
 
 
 if __name__ == "__main__":
     # Expected default file location: ./secret/keys.toml
-    read_from = FromFileTOML(key="tinkoff")
-    data = read_key(read_from)
-    print("key obtained:", data["token"])
+    tinkoff = get_from_toml("tinkoff")
+    print("key obtained:", tinkoff["token"])
+    # print("key obtained:", get_from_env("TOKEN"))
