e919ba5a501b3883211c644ce545fcaf9a448fe5
Vic Khov <victorkhovtko@gmail.com>
2023-07-12 21:00:32 +0300

    UNTESTED CODE: updated main bot module

--- bot/app.py
+++ bot/app.py
@@ -1,9 +1,9 @@
 from pyrogram import filters, Client as PyrogramClient
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery
 from pyrogram.errors import UserNotParticipant
 from creds import get_from_toml
 from supabase import create_client, Client as DbClient
-
+from stocks_alpha_vantage import get_stock_info
 
 BOT_NAME = "stonks-bot"
 
@@ -67,14 +67,57 @@ def track_stock(client: PyrogramClient, message: Message):
 
     stock_ticker, price_to_be_reached = args
 
-    # save tracking details to Supabase linked to user's telegram account
-    supabase.table("tracking").insert({
-        "user_id": user_id,
-        "stock_ticker": stock_ticker,
-        "price_to_be_reached": price_to_be_reached
-    }).execute()
+    # retrieve stock info using get_stock_info
+    stock_info = get_stock_info(stock_ticker)
 
-    message.reply("Stock tracking started successfully.")
+    if stock_info is None:
+        message.reply(
+            "Failed to retrieve stock data. Try searching stocks with command '/search <ticker>'.")
+        return
+
+    # create reply message with stock info and a button to confirm tracking
+    reply_message = f"Stock information:\n\n" \
+                    f"Ticket: {stock_info.ticket}\n" \
+                    f"Price: {stock_info.price}\n" \
+                    f"Exchange: {stock_info.exchange}\n" \
+                    f"Stock Real Name: {stock_info.stock_real_name}\n" \
+                    f"Percentage Delta: {stock_info.per_day_price_delta_percentage}%\n\n" \
+                    f"If this is the correct stock you want to track, click the button below to confirm."
+
+    keyboard = InlineKeyboardMarkup(
+        [[InlineKeyboardButton(
+            "Confirm Tracking", callback_data=f"confirm_track_{stock_ticker}-{price_to_be_reached}")]]
+    )
+
+    message.reply_text(reply_message, reply_markup=keyboard)
+
+
+@app.on_callback_query()
+def handle_button_click(client: PyrogramClient, callback_query: CallbackQuery):
+    """Handler for button callbacks
+    """
+
+    user_id = callback_query.from_user.id
+    data = callback_query.data
+    if data.startswith("confirm_track_"):
+        prefix, price = data.split("-")
+        stock_ticker = prefix.replace("confirm_track_", "")
+
+        # retrieve stock info using get_stock_info
+        stock_info = get_stock_info(stock_ticker)
+
+        if stock_info is None:
+            callback_query.answer("Failed to retrieve stock data.")
+            return
+
+        # save tracking details to Supabase linked to user's telegram account
+        supabase.table("tracking").insert({
+            "user_id": user_id,
+            "stock_ticker": stock_ticker,
+            "price_to_be_reached": price
+        }).execute()
+
+        callback_query.answer("Stock tracking started successfully.")
 
 
 # run the bot
