94c060cf8c30871edb6e3db4d7cdc932cededb13
Vic Khov <victorkhovtko@gmail.com>
2023-07-12 20:25:08 +0300

    fixed typos, test stock data provider: alpha-vantage API

--- bot/stocks.py
+++ bot/stocks.py
@@ -2,10 +2,11 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 from alpha_vantage.timeseries import TimeSeries
 from creds import get_from_toml
+from stocks_alpha_vantage import get_currency_pair_info, get_current_stock_info
 
 
-if __name__ == "__main__":
-    TICKET = "MSFT"
+def test_small():
+    TICKER = "MSFT"
 
     alpha_vantage = get_from_toml("alpha-vantage")["token"]
     print(alpha_vantage)
@@ -13,7 +14,7 @@ if __name__ == "__main__":
     ts = TimeSeries(key=alpha_vantage, output_format="pandas")
 
     data, meta_data = ts.get_intraday(
-        symbol=TICKET,
+        symbol=TICKER,
         interval="1min",
         outputsize="full"
     )
@@ -23,5 +24,17 @@ if __name__ == "__main__":
 
     stamp = datetime.now().strftime('%d-%m-%Y_%H-%M-%S')
 
-    plt.savefig(f"./data/{TICKET}_{stamp}.png")
+    plt.savefig(f"./data/{TICKER}_{stamp}.png")
     print("figure saved!")
+
+
+def test_functions():
+    ticker_info = get_current_stock_info("MSFT")
+    print(ticker_info)
+
+    curr_info = get_currency_pair_info("USD", "JPY")
+    print(curr_info)
+
+
+if __name__ == "__main__":
+    test_functions()
