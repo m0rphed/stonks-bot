55a3116e0c3406f30e41f017f2c25801e6b74ca7
Vic Khov <victorkhovtko@gmail.com>
2023-07-13 13:57:06 +0300

    added annotations

--- bot/creds.py
+++ bot/creds.py
@@ -3,7 +3,7 @@ import toml
 from dotenv import load_dotenv
 
 
-def get_from_toml(key: str, toml_path: str = "./secret/keys.toml"):
+def get_from_toml(key: str, toml_path: str = "./secret/keys.toml") -> str | None:
     # get the file path
     file_path = os.path.join(toml_path)
     if not os.path.exists(file_path):
@@ -18,7 +18,7 @@ def get_from_toml(key: str, toml_path: str = "./secret/keys.toml"):
     return secrets[key]  # return specified secret key
 
 
-def load_env_file(env_file_path: str):
+def load_env_file(env_file_path: str) -> None:
     if not env_file_path:
         raise ValueError("Trying to load `.env` file: got empty str as path")
 
@@ -28,7 +28,7 @@ def load_env_file(env_file_path: str):
     load_dotenv(dotenv_path=env_file_path)
 
 
-def get_from_env(key: str, env_file_path: str | None = None):
+def get_from_env(key: str, env_file_path: str | None = None) -> str | None:
     if env_file_path is not None:
         # take environment variables from .env file
         load_dotenv(dotenv_path=env_file_path)
