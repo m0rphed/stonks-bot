f15835d82ed3be1d303df678174d743cf383dceb
Vic Khov <victorkhovtko@gmail.com>
2023-07-16 23:55:48 +0300

    fix: wrong decorator implementation

--- bot/db_funcs.py
+++ bot/db_funcs.py
@@ -7,54 +7,35 @@ SupportedDB = SupabaseClient
 
 
 def get_supabase_client() -> SupabaseClient:
-    # TODO: find a way to inject config & then do: `if config.load_env_flag: ...`
-    creds.load_env_file("./secret/.env")
     sb_url = creds.get_from_env("SUPABASE_URL")
     # TODO: find a way to inject config & then do: `if config.use_supabase_public: ...`
     sb_key = creds.get_from_env("SUPABASE_SEC_KEY")
     return create_client(sb_url, sb_key)
 
 
-def with_supabase():
-    # TODO: find a way to inject config & then do: `if config.load_env_flag: ...`
+def with_supabase(func):
     creds.load_env_file("./secret/.env")
     sb_url = creds.get_from_env("SUPABASE_URL")
     # TODO: find a way to inject config & then do: `if config.use_supabase_public: ...`
     sb_key = creds.get_from_env("SUPABASE_SEC_KEY")
-
     supabase = create_client(sb_url, sb_key)
 
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            # Validate arguments
-            if not isinstance(args[0], SupabaseClient):
-                raise ValueError(
-                    "First argument must be an instance of `supabase.Client`")
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        return func(supabase, *args, **kwargs)
 
-            return func(supabase, *args, **kwargs)
-
-        return wrapper
-
-    return decorator
+    return wrapper
 
 
 def with_supabase_config(supabase_url: str, supabase_key: str):
     supabase = create_client(supabase_url, supabase_key)
 
-    def decorator(func):
+    def decorator_with_supabase_config(func):
         @wraps(func)
-        def wrapper(*args, **kwargs):
-            # Validate arguments
-            if not isinstance(args[0], SupabaseClient):
-                raise ValueError(
-                    "First argument must be an instance of `supabase.Client`")
-
+        def wrapper_with_supabase_config(*args, **kwargs):
             return func(supabase, *args, **kwargs)
-
-        return wrapper
-
-    return decorator
+        return wrapper_with_supabase_config
+    return decorator_with_supabase_config
 
 # TODO: consider adding functions that do not relay on specific database
 # but can do operations with whatever database provider (that specified by parameter passed to function)
