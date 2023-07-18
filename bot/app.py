5375e3c372f21f368db553ab2d88daf817859db1
Vic Khov <victorkhovtko@gmail.com>
2023-07-18 05:08:37 +0300

    [wip] changed bot & db funcs behavior

--- bot/app.py
+++ bot/app.py
@@ -1,12 +1,14 @@
 from pyrogram import filters, Client as PyrogramClient
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery
-from pyrogram import enums as pyro_enums
+from pyrogram import enums
 
 import creds
+import json
 
-from models import CurrencyPairInfo, InstrumentSearchInfo
+from models import BotUserEntity, CurrencyPairInfo, InstrumentEntity, InstrumentSearchInfo, StockInfo
 from api_alpha_vantage import get_curr_pair_info, get_stock_info, get_search_results, instrument_to_markdown
 from db_funcs import get_supabase_client
+from supabase_funcs import add_instrument, add_user_by_id, check_instrument_by_ticker, check_user, add_tracking
 
 BOT_NAME = "stonks-bot"
 
@@ -24,73 +26,182 @@ app = PyrogramClient(
 supabase = get_supabase_client()
 
 
+def msg_warning(msg: str):
+    return "⚠️ " + msg
+
+
+def msg_error(msg: str):
+    return "⛔ " + msg
+
+
+def msg_ok(msg: str):
+    return "✅ " + msg
+
+
+async def authenticated_users_only(tg_user_id: int, chat_id: int) -> BotUserEntity | None:
+    user = check_user(tg_user_id)
+    if user is None:
+        await app.send_message(
+            chat_id,
+            msg_warning("You are NOT authenticated to do this.")
+        )
+        return None
+
+    return user
+
+
 @app.on_message(filters.command("auth"))
-async def authenticate_user(client: PyrogramClient, message: Message):
-    """Handler for "/auth" command
+async def auth_user(client: PyrogramClient, message: Message):
+    """User authentication command
     """
 
-    # telegram's user id is supposed to be unique (not affected by username change etc.)
+    # user_id is unique & not affected by username change etc.
     user_id = message.from_user.id
 
     # check if the user is already authenticated
-    user = supabase.table("bot_users").select("*").eq(
-        "tg_user_id", user_id).execute()
-
-    if len(user.data) > 0:
-        await message.reply("⚠️ You are already authenticated.")
+    user = check_user(user_id)
+    if user is not None:
+        await message.reply(msg_warning("You are already authenticated."))
         return
 
-    # save user's unique id to DB
-    supabase.table("bot_users").insert({"tg_user_id": user_id}).execute()
-    await message.reply("✅ Authentication successful!")
+    user_added = add_user_by_id(user_id)
+
+    if user_added is not None:
+        await message.reply(msg_ok("Authentication successful!"))
+        return
+
+    await message.reply(
+        msg_error("Authentication failed for your telegram account;\n"
+                  "try `/erase_me` - then try again."),
+        parse_mode=enums.ParseMode.MARKDOWN
+    )
+
+
+@app.on_message(filters.command("portfolio"))
+async def show_portfolio(client: PyrogramClient, message: Message):
+    """shows portfolio of the user: tracked equities, currency pairs, crypto pairs 
+    """
+    raise NotImplementedError("ups, not done this part yet :D")
 
 
 @app.on_message(filters.command("track_stock"))
 async def track_stock(client: PyrogramClient, message: Message):
-    """Handler for "/track_stock <stock_ticker> <price_to_be_reached>" command
+    """adds new tracking of a stock: "/track_stock <stock_ticker> <price>"
     """
+    user_db_obj: BotUserEntity | None = authenticated_users_only(
+        tg_user_id=message.from_user.id,
+        chat_id=message.chat.id
+    )
+
+    if user_db_obj is None:
+        return
 
     args = message.text.split()[1:]
     if len(args) != 2:
-        await message.reply("Please provide a stock ticker and price to be reached.")
+        await message.reply(
+            msg_error(
+                "Please provide a stock ticker and price to be reached.")
+        )
         return
 
-    stock_ticker, price_str = args
-
+    ticker, price_str = args
     try:
-        # exception would be raised if string could not be converted to valid float value
+        # exception would be raised
+        # if string could not be converted to valid float value
         price_to_be_reached = float(price_str)
     except ValueError:
-        await message.reply(f"⛔ Price you provided: '{price_str}'\n- Please provide a valid price value e.g. 👉 142, 255.5, 0.034")
-        return
-
-    # retrieve stock info using get_stock_info
-    stock_info = await get_stock_info(stock_ticker)
-
-    if stock_info is None:
         await message.reply(
-            "⛔ Failed to retrieve stock data. Try searching stocks with command '/search_stock <ticker>'.")
+            msg_error(
+                f"Price you provided: '{price_str}'\n"
+                "- Please provide a valid price value"
+                " e.g. 👉 142, 255.5, 0.034")
+        )
         return
 
-    # create reply message with stock info and a button to confirm tracking
-    reply_message = f"Stock information:\n\n" \
-                    f"Ticket: {stock_info.ticker}\n" \
-                    f"Price: {stock_info.price}\n" \
-                    f"Exchange: {stock_info.exchange}\n" \
-                    f"Stock Real Name: {stock_info.stock_real_name}\n" \
-                    f"Percentage Delta: {stock_info.per_day_price_delta_percentage}%\n\n" \
-                    f"If this is the correct stock you want to track, click the button below to confirm."
+    # if all correct - we prepare to track new instrument:
+    #   by retrieving it from "instruments" table, and then adding it to "tracking" table,
+    #   or by requesting it from preferred API - then adding both to "instruments" and to "tracking" tables
+    new_stock: None | dict = None
+    tracking: None | dict = None
+    reply_message: None | str = None
+
+    # check if stock ticker is tracked by anyone
+    # (which means that its ticker exist in the "instruments" table)
+    stock_db_obj: InstrumentEntity | None = await check_instrument_by_ticker(ticker, "alpha_vantage")
+    if stock_db_obj is not None:
+        tracking = {
+            "tracked_instrument": stock_db_obj.id,
+            "tracked_by_user": user_db_obj.id,
+            "notify": "on_change",
+            "on_price": float(price_to_be_reached)
+        }
+
+        # create reply message with stock info from db
+        reply_message = f"Stock information:\n\n" \
+                        f"Ticket: {stock_db_obj.ticker}\n" \
+                        f"Price: {stock_db_obj.price}\n" \
+                        f"Exchange: {stock_db_obj.code_exchange}\n" \
+                        f"👉 provided by: `alpha_vantage`\n" \
+                        f"\nIf this is the correct stock you want to track, click the button below to confirm."
+
+    # if not found in "instruments" -> try retrieve from API
+    else:
+        # retrieve stock info using get_stock_info
+        stock_api: StockInfo | None = await get_stock_info(ticker)
+        if stock_api is None:
+            await message.reply(
+                msg_error(
+                    f"Failed to retrieve stock data by provided symbol: {ticker}.\n"
+                    "Try searching stocks with command '/search_stock <ticker>' first.")
+            )
+            return
+
+        # prepare fields of new instrument that to be inserted to the table
+        new_stock: dict = {
+            "is_curr_pair": False,
+            "is_crypto_pair": False,
+            "data_provider": "alpha_vantage",
+            "price": stock_api.price,
+            "code_exchange": stock_api.exchange,
+            "ticker": stock_api.symbol
+        }
+
+        tracking = {
+            # link to the new instrument we've just inserted
+            "tracked_instrument": "",
+            "tracked_by_user": user_db_obj.id,
+            "notify": "on_change",
+            "on_price": float(price_to_be_reached)
+        }
+
+        # create reply message with stock info from API
+        reply_message = f"Stock information:\n\n" \
+                        f"Ticket: {stock_api.symbol}\n" \
+                        f"Price: {stock_api.price}\n" \
+                        f"Exchange: {stock_api.exchange}\n" \
+                        f"Change: {stock_api.change}\n" \
+                        f"Percentage change: {stock_api.change_percent}\n" \
+                        f"👉 provided by: `alpha_vantage`\n" \
+                        f"\nIf this is the correct stock you want to track, click the button below to confirm."
+
+    # prepare data to pass to the callback
+    data = {
+        "new_instrument": "" if new_stock is None else new_stock,
+        "tracking": tracking
+    }
 
     keyboard = InlineKeyboardMarkup(
         [[InlineKeyboardButton(
-            "Confirm", callback_data=f"tracking_confirmed_{stock_ticker}-{price_to_be_reached}"
+            "Confirm", callback_data=f"tracking_stock_confirmed>{ticker}-{price_to_be_reached}>{json.dumps(data)}"
+        )], [InlineKeyboardButton(
+            "❌ Cancel", callback_data=f"tracking_stocked_canceled"
         )]]
     )
 
     await message.reply_text(
         reply_message,
         reply_markup=keyboard,
-        parse_mode=pyro_enums.ParseMode.MARKDOWN
+        parse_mode=enums.ParseMode.MARKDOWN
     )
 
 
@@ -101,7 +212,8 @@ async def track_currency(client: PyrogramClient, message: Message):
         /track USD EUR
         /track BTC USD
     """
-
+    raise NotImplementedError(
+        "have not rewritten to work with new sb functions patch")
     args = message.text.split()[1:]
     if len(args) != 2:
         await message.reply("Please provide a stock ticker and price to be reached.")
@@ -135,108 +247,79 @@ async def track_currency(client: PyrogramClient, message: Message):
     await message.reply_text(
         reply_message,
         reply_markup=keyboard,
-        parse_mode=pyro_enums.ParseMode.MARKDOWN
+        parse_mode=enums.ParseMode.MARKDOWN
     )
 
 
 @app.on_message(filters.command("search_stock"))
 async def search_stock(client: PyrogramClient, message: Message):
-    """Handler for "/search_stock <search query>" command
+    """searches for stock or any equity covered by the API
+    - example: "/search_stock <search query>" command
     """
 
     query = message.text.replace("/search_stock", "").strip()
     if query is None or query == "" or query == " ":
-        await message.reply("Please provide a stock ticker or a message to search through available instruments")
+        await message.reply(msg_warning("Please provide a stock ticker or a message to search through available instruments"))
         return
 
     # retrieve available stocks, bond, currencies
     xs: list[InstrumentSearchInfo] | None = await get_search_results(query)
 
     if xs is None or xs == []:
-        await message.reply(
-            "Failed to retrieve instrument data. Try searching with different keywords.")
+        await message.reply(msg_error("Failed to retrieve instrument data. Try searching with different keywords."))
         return
 
     for inst in xs:
-        await message.reply(instrument_to_markdown(inst), parse_mode=pyro_enums.ParseMode.MARKDOWN)
+        await message.reply(instrument_to_markdown(inst), parse_mode=enums.ParseMode.MARKDOWN)
 
 
-@app.on_callback_query()
+@app.on_callback_query
 async def handle_button_click(client: PyrogramClient, callback_query: CallbackQuery):
     """Handler for button callbacks
     """
 
-    user_id = callback_query.from_user.id
     data = callback_query.data
-
     if data.startswith("tracking_currency_confirmed>"):
-        curr_codes: list = data.replace(
-            "tracking_currency_confirmed>", "").split("-")
-        curr_info: CurrencyPairInfo | None = await get_curr_pair_info(curr_codes[0], curr_codes[1])
+        raise NotImplementedError("not yet done :D")
 
-        # TODO: remove unnecessary api request before adding to db in callback
-        if curr_info is None:
-            await callback_query.answer("⛔ Failed to retrieve stock data.")
+    if data.startswith("tracking_stock_confirmed>"):
+        _, ticker_and_price, data_json = data.split(">")
+        ticker, price = ticker_and_price.split("-")
+        data_obj = json.loads(data_json)
+
+        new_instrument = data_obj["new_instrument"]
+        tracking = data_obj["tracking"]
+
+        # if 'new_instrument' passed
+        #   - we need to add new instrument to 'instruments' table
+        #       and after that - add new 'tracking'
+        if isinstance(new_instrument, dict):
+            # insert new instrument to DB
+            instrument_db_obj = add_instrument(new_instrument)
+            if instrument_db_obj is None:
+                await callback_query.answer(msg_error("Failed to add new tracking"))
+                raise RuntimeError(
+                    f"Failed to insert new instrument {new_instrument}")
+
+            tracking["tracked_instrument"] = instrument_db_obj["id"]
+            tracking_db_obj = add_tracking(tracking)
+
+            if tracking_db_obj in None:
+                await callback_query.answer(msg_error("Failed to add new tracking"))
+                raise RuntimeError(f"Failed to insert new tracking {tracking}")
+
+            await callback_query.answer(msg_ok(f"Added {ticker} notify on price: {price}"))
             return
 
-        try:
-            # TODO: check if currency pair is in INSTRUMENTS
-            db_curr_pair = supabase.table("instruments").select("*"
-                                                                ).eq("is_curr_pair", True
-                                                                     ).eq("data_provider", "alpha_vantage"
-                                                                          ).eq("code_curr", f"{curr_info.code_from}-{curr_info.code_to}"
-                                                                               ).execute()
+        # if 'new_instrument' was not passed
+        # (means that it's already added to table 'instruments')
+        #   - so we only should add new 'tracking'
+        tracking_obj = add_tracking(tracking)
+        if tracking_obj in None:
+            await callback_query.answer(msg_error("Failed to add new tracking"))
+            raise RuntimeError(f"Failed to insert new tracking {tracking}")
 
-            if len(db_curr_pair.data) == 1:
-                row = db_curr_pair.data[0]
-                raise NotImplementedError(
-                    "That means that this currency pair is tracked by at least 1 user"
-                    "-> We need to get its `table('instruments').id`"
-                    "-> We need to get our `table('bot_users').id` which == table('bot_users').select('*').eq('tg_user_id', user_id)"
-                    "-> We need to define how frequent we want to receive messages = 'daily', 'on_change', ...")
-
-            if len(db_curr_pair.data) == 0:
-                raise NotImplementedError(
-                    "No such currency pair tracked by anyone -> you need to upd INSTRUMENTS")
-
-            if len(db_curr_pair.data) > 1:
-                raise NotImplementedError("Impossible case, table corrupted")
-
-            # save tracking details to Supabase linked to user's telegram account
-            supabase.table("tracking").insert({
-                "user_id": user_id,
-                "ticker": stock_ticker,
-                "on_price_value": on_price_value
-            }).execute()
-
-        except Exception as e:  # TODO: handle try adding already tracking stock
-            await callback_query.answer(f"⛔ Tracking failed: {e}")
-            return
-
-    if data.startswith("tracking_confirmed_"):
-        prefix, on_price_value = data.split("-")
-        stock_ticker = prefix.replace("tracking_confirmed_", "")
-
-        # TODO: remove unnecessary api request before adding to db in callback
-        stock_info = await get_stock_info(stock_ticker)
-
-        if stock_info is None:
-            await callback_query.answer("⛔ Failed to retrieve stock data.")
-            return
-
-        try:
-            # save tracking details to Supabase linked to user's telegram account
-            supabase.table("tracking").insert({
-                "user_id": user_id,
-                "ticker": stock_ticker,
-                "on_price_value": on_price_value
-            }).execute()
-
-        except Exception as e:  # TODO: handle try adding already tracking stock
-            await callback_query.answer(f"⛔ Tracking failed: {e}")
-            return
-
-        await callback_query.answer("✅ Tracking started successfully.")
+        await callback_query.answer(msg_ok(f"Added {ticker} notify on price: {price}"))
 
 
 # run the bot
--- bot/supabase_funcs.py
+++ bot/supabase_funcs.py
@@ -4,9 +4,9 @@ from db_funcs import with_supabase, with_supabase_config
 
 
 @with_supabase
-def check_user(supabase: SupabaseClient, user_id: str) -> BotUserEntity | None:
+def check_user(supabase: SupabaseClient, tg_user_id: int) -> BotUserEntity | None:
     resp = supabase.table("bot_users").select("*"
-        ).eq("tg_user_id", user_id).execute()
+        ).eq("tg_user_id", tg_user_id).execute()
 
     if len(resp.data) > 0:
         if len(resp.data) == 1:
@@ -14,8 +14,8 @@ def check_user(supabase: SupabaseClient, user_id: str) -> BotUserEntity | None:
 
         # TODO: impl. & raise something like `DBError(...)`
         raise Exception(
-            f"DB have multiple users with the same `tg_bot_id`:" \
-                f" {user_id} -> data must be corrupted")
+            f"DB have multiple users with the same `tg_bot_id`:"
+            f" {tg_user_id} -> data must be corrupted")
 
     return None
 
@@ -30,12 +30,12 @@ def check_curr_pair(supabase: SupabaseClient, code_from: str, code_to: str, data
     if len(resp.data) > 0:
         if len(resp.data) == 1:
             return InstrumentEntity.parse_obj(resp.data[0])
-        
+
         # TODO: impl. & raise something like `DBError(...)`
         raise Exception(
-            f"DB have multiple currency pair with the same `code_curr`:" \
-                f" {code_from}_{code_to} -> data must be corrupted")
-    
+            f"DB have multiple currency pair with the same `code_curr`:"
+            f" {code_from}_{code_to} -> data must be corrupted")
+
     raise None
 
 
@@ -49,17 +49,17 @@ def check_crypto_pair(supabase: SupabaseClient, code_from: str, code_to: str, da
     if len(resp.data) > 0:
         if len(resp.data) == 1:
             return InstrumentEntity.parse_obj(resp.data[0])
-        
+
         # TODO: impl. & raise something like `DBError(...)`
         raise Exception(
-            f"DB have multiple crypto currency pair with the same `code_curr`:" \
-                f" {code_from}_{code_to} -> data must be corrupted")
-    
+            f"DB have multiple crypto currency pair with the same `code_curr`:"
+            f" {code_from}_{code_to} -> data must be corrupted")
+
     raise None
 
 
 @with_supabase
-def check_by_figi(supabase: SupabaseClient, code_figi: str, data_provider: str) -> InstrumentEntity | None:
+def check_instrument_by_figi(supabase: SupabaseClient, code_figi: str, data_provider: str) -> InstrumentEntity | None:
     resp = supabase.table("instruments").select("*"
         ).eq("is_crypto_pair", False    # TODO: does adding constrains in such case improve perf.? 
         ).eq("is_curr_pair", False      # TODO: does adding constrains in such case improve perf.?
@@ -69,13 +69,13 @@ def check_by_figi(supabase: SupabaseClient, code_figi: str, data_provider: str) 
     if len(resp.data) > 0:
         if len(resp.data) == 1:
             return InstrumentEntity.parse_obj(resp.data[0])
-        
+
         # TODO: impl. & raise something like `DBError(...)`
         raise Exception(
-            f"DB have multiple instruments with the same `code_figi`:" \
-                f" {code_figi} (`data_provider`: {data_provider})" \
-                    "-> data must be corrupted")
-    
+            f"DB have multiple instruments with the same `code_figi`:"
+            f" {code_figi} (`data_provider`: {data_provider})"
+            "-> data must be corrupted")
+
     raise None
 
 
@@ -90,49 +90,76 @@ def check_instrument_by_ticker(supabase: SupabaseClient, ticker: str, data_provi
     if len(resp.data) > 0:
         if len(resp.data) == 1:
             return InstrumentEntity.parse_obj(resp.data[0])
-        
+
         # TODO: impl. & raise something like `DBError(...)`
         raise Exception(
-            f"DB have multiple instruments with the same `ticker`:" \
-                f" {ticker} (`data_provider`: {data_provider})" \
-                    "-> data must be corrupted")
-    
+            f"DB have multiple instruments with the same `ticker`:"
+            f" {ticker} (`data_provider`: {data_provider})"
+            "-> data must be corrupted")
+
     raise None
 
 
 @with_supabase
-def insert_instrument(supabase: SupabaseClient, instrument: InstrumentEntity) -> bool:
+def add_user_by_id(supabase: SupabaseClient, tg_user_id: int) -> dict | None:
     try:
         # TODO: does response need additional handling in such cases?
-        _resp, _ = supabase.table("instruments").insert(instrument.dict()).execute()
-        return True
-        
+        resp = supabase.table("bot_users").insert(
+            {"tg_user_id": tg_user_id}).execute()
+        return resp.data[0]
+
     except Exception as e:
         # TODO: log error
-        return False
+        return None
 
 
 @with_supabase
-def insert_tracking(supabase: SupabaseClient, tracking: TrackingEntity) -> bool:
+def add_user(supabase: SupabaseClient, bot_user_fields: dict) -> dict | None:
     try:
         # TODO: does response need additional handling in such cases?
-        _resp, _ = supabase.table("tracking").insert(tracking.dict()).execute()
-        return True
-        
+        resp = supabase.table("bot_users").insert(
+            bot_user_fields).execute()
+        return resp.data[0]
+
     except Exception as e:
         # TODO: log error
-        return False
-    
+        return None
+
 
 @with_supabase
-def get_trackings(supabase: SupabaseClient, tracked_by_tg_user: int) -> list[TrackingEntity] | None:
+def add_instrument(supabase: SupabaseClient, instrument_fields: dict) -> dict | None:
+    try:
+        # TODO: does response need additional handling in such cases?
+        resp = supabase.table("instruments").insert(
+            instrument_fields).execute()
+        return resp.data[0]
+
+    except Exception as e:
+        # TODO: log error
+        return None
+
+
+@with_supabase
+def add_tracking(supabase: SupabaseClient, tracking_fields: dict) -> dict | None:
+    try:
+        # TODO: does response need additional handling in such cases?
+        resp = supabase.table("tracking").insert(tracking_fields).execute()
+        return resp.data[0]
+
+    except Exception as e:
+        # TODO: log error
+        return None
+
+
+@with_supabase
+def get_trackings(supabase: SupabaseClient, tracked_by_user: int) -> list[TrackingEntity] | None:
     resp = supabase.table("tracking"
-            ).select("*, bot_users(*)"
-            ).eq("bot_users.tg_user_id", tracked_by_tg_user).execute()
+                          ).select("*, bot_users(*)"
+                                   ).eq("bot_users.tg_user_id", tracked_by_user).execute()
     print(resp.data)
     raise NotImplementedError("Implementation not ready")
     # return None
-    
+
 
 def _test():
     pass
