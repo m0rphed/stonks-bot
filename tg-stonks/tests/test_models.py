a0efdcc9ac4520ae88af7301155bd28fe18c12a1
Vic Khov <victorkhovtko@gmail.com>
2023-08-16 23:32:11 +0300

    added 1 simple test to see if coverage works

---
+++ tg-stonks/tests/test_models.py
@@ -0,0 +1,23 @@
+import tg_stonks.utils.creds as creds
+from tg_stonks.impl.alpha_vantage_provider import AlphaVantageAPI
+import pprint
+
+import pytest
+
+pytest_plugins = ('pytest_asyncio',)
+
+
+@pytest.mark.asyncio
+async def test_search_stock_market_not_empty():
+    creds.load_env_file("../secret/.env")
+    ticker = "AAPL"
+    av_api = AlphaVantageAPI(creds.get_from_env("ALPHA_VANTAGE_TOKEN"))
+    query_res = await av_api.search_stock_market(query=ticker)
+    dumped = []
+    for res in query_res:
+        dumped.append(res.model_dump(mode="python"))
+        assert res.data_provider == av_api.data_provider_name
+
+    assert not len(query_res) == 0
+    for x in dumped:
+        pprint.pprint(x)
