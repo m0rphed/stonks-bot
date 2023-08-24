26698d52c95af7a2f29e4df08bd34546cf0ef4d1
Vic Khov <victorkhovtko@gmail.com>
2023-08-24 12:42:35 +0300

    simplified and generalized formatting

--- tg-stonks/src/tg_stonks/bot/helpers.py
+++ tg-stonks/src/tg_stonks/bot/helpers.py
@@ -6,41 +6,44 @@ from tg_stonks.database.user_settings import DataProviderConfig
 from tg_stonks.bot.formatting import msg_error
 
 
-def confirmation_markup(cb_data_confirmed: str, cb_data_canceled: str) -> InlineKeyboardMarkup:
-    return InlineKeyboardMarkup(
-        [[
-            InlineKeyboardButton(
-                "✅ Confirm", callback_data=cb_data_confirmed
-            ),
-            InlineKeyboardButton(
-                "❌ Cancel", callback_data=cb_data_canceled
-            )
-        ]]
-    )
-
-
-def cancel_btn(callback_data_suffix: str) -> InlineKeyboardButton:
+def btn_cancel(callback_data_suffix: str) -> InlineKeyboardButton:
     return InlineKeyboardButton(
         "❌ Cancel",
         callback_data=f"canceled {callback_data_suffix}"
     )
 
 
-def set_prov_btn(btn_text: str, prov_t: str, prov_name: str) -> InlineKeyboardButton:
+def btn_confirm(cb_data_suffix: str) -> InlineKeyboardButton:
     return InlineKeyboardButton(
-        btn_text,
-        callback_data=f"confirmed --cmd prov {prov_t} {prov_name}"
+        "✅ Confirm",
+        callback_data=cb_data_suffix
     )
 
 
-def chose_provider_markup(prov: IDataProvider) -> tuple[str, InlineKeyboardMarkup]:
+def btn_set_prov(btn_text: str, prov_t_str: str, prov_name: str) -> InlineKeyboardButton:
+    return InlineKeyboardButton(
+        btn_text,
+        callback_data=f"confirmed --cmd prov {prov_t_str} {prov_name}"
+    )
+
+
+def markup_confirmation(cb_data_confirmed: str, cb_data_canceled: str) -> InlineKeyboardMarkup:
+    return InlineKeyboardMarkup(
+        [[
+            btn_confirm(cb_data_confirmed),
+            btn_cancel(cb_data_canceled)
+        ]]
+    )
+
+
+def markup_chose_provider(prov: IDataProvider) -> tuple[str, InlineKeyboardMarkup]:
     match prov.provider_type:
         case ProviderT.STOCK_MARKET:
             msg = "Set this provider for --stock market-- data 🚀📈🦬"
             markup = InlineKeyboardMarkup(
                 [[
-                    set_prov_btn("Set for stock market", ProviderT.STOCK_MARKET.value, prov.provider_name),
-                    cancel_btn("--cmd prov")
+                    btn_set_prov("Set for stock market", ProviderT.STOCK_MARKET.value, prov.provider_name),
+                    btn_cancel("--cmd prov")
                 ]]
             )
             return msg, markup
@@ -49,8 +52,8 @@ def chose_provider_markup(prov: IDataProvider) -> tuple[str, InlineKeyboardMarku
             msg = "Set this provider for --foreign currency exchange-- data 💹💲💱"
             markup = InlineKeyboardMarkup(
                 [[
-                    set_prov_btn("Set for forex market", ProviderT.CURR_FOREX.value, prov.provider_name),
-                    cancel_btn("--cmd prov")
+                    btn_set_prov("Set for forex market", ProviderT.CURR_FOREX.value, prov.provider_name),
+                    btn_cancel("--cmd prov")
                 ]]
             )
             return msg, markup
@@ -59,8 +62,8 @@ def chose_provider_markup(prov: IDataProvider) -> tuple[str, InlineKeyboardMarku
             msg = "Set this provider for --cryptocurrency-- data ₿👩🏻‍💻🌐"
             markup = InlineKeyboardMarkup(
                 [[
-                    set_prov_btn("Set for crypto market", ProviderT.CURR_CRYPTO.value, prov.provider_name),
-                    cancel_btn("--cmd prov")
+                    btn_set_prov("Set for crypto market", ProviderT.CURR_CRYPTO.value, prov.provider_name),
+                    btn_cancel("--cmd prov")
                 ]]
             )
             return msg, markup
@@ -69,49 +72,51 @@ def chose_provider_markup(prov: IDataProvider) -> tuple[str, InlineKeyboardMarku
             msg = "You can set this provider for --all kinds-- of data 📈💹₿"
             markup = InlineKeyboardMarkup(
                 [
-                    [set_prov_btn("Set for stock market", ProviderT.STOCK_MARKET.value, prov.provider_name)],
-                    [set_prov_btn("Set for forex market", ProviderT.CURR_FOREX.value, prov.provider_name)],
-                    [set_prov_btn("Set for crypto market", ProviderT.CURR_CRYPTO.value, prov.provider_name)],
-                    [set_prov_btn("Set for everything", ProviderT.UNIVERSAL.value, prov.provider_name)],
-                    [cancel_btn("--cmd prov")]
+                    [btn_set_prov("Set for stock market", ProviderT.STOCK_MARKET.value, prov.provider_name)],
+                    [btn_set_prov("Set for forex market", ProviderT.CURR_FOREX.value, prov.provider_name)],
+                    [btn_set_prov("Set for crypto market", ProviderT.CURR_CRYPTO.value, prov.provider_name)],
+                    [btn_set_prov("Set for everything", ProviderT.UNIVERSAL.value, prov.provider_name)],
+                    [btn_cancel("--cmd prov")]
                 ]
             )
             return msg, markup
 
 
-async def _running_without_providers(message: Message):
-    await message.reply(
-        msg_error(
-            "Bot running without any data providers passed"
-        )
-    )
+async def reply_running_without_providers(message: Message):
+    await message.reply(msg_error("Bot running without any data providers"))
 
 
-def _providers_settings2(prov_t_value: str, prov_conf: DataProviderConfig) -> dict:
+def make_prov_settings(prov_t_value: ProviderT, conf: DataProviderConfig) -> dict:
     match prov_t_value:
         case ProviderT.UNIVERSAL:
             return {
-                "provider_stock_market": prov_conf.dict(),
-                "provider_currency": prov_conf.dict(),
-                "provider_crypto": prov_conf.dict()
+                "provider_stock_market": conf.model_dump(),
+                "provider_currency": conf.model_dump(),
+                "provider_crypto": conf.model_dump()
             }
         case ProviderT.CURR_CRYPTO:
             return {
                 "provider_stock_market": None,
                 "provider_currency": None,
-                "provider_crypto": prov_conf.dict()
+                "provider_crypto": conf.model_dump()
             }
 
         case ProviderT.CURR_FOREX:
             return {
                 "provider_stock_market": None,
-                "provider_currency": prov_conf.dict(),
+                "provider_currency": conf.model_dump(),
                 "provider_crypto": None
             }
 
         case ProviderT.STOCK_MARKET:
             return {
-                "provider_stock_market": prov_conf.dict(),
+                "provider_stock_market": conf.model_dump(),
                 "provider_currency": None,
                 "provider_crypto": None
             }
+
+        case _:
+            raise ValueError(
+                "Specified provider type is incorrect",
+                prov_t_value
+            )
