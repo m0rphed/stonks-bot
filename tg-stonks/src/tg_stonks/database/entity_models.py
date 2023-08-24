c87eba9b26884fec63fa383786d13d010b8072d8
Vic Khov <victorkhovtko@gmail.com>
2023-08-24 12:38:30 +0300

    added doc-strings to entity models

--- tg-stonks/src/tg_stonks/database/entity_models.py
+++ tg-stonks/src/tg_stonks/database/entity_models.py
@@ -8,20 +8,46 @@ from pydantic import BaseModel, Field
 
 @unique
 class InstrumentType(StrEnum):
+    """
+    Enum defines different types of financial instruments:
+    - 'stock_market_instrument' is for anything related to stock market e.g.
+        any securities like: equities (stocks, shares), bonds etc.
+    - 'currency_exchange_pair' is for foreign currency to crypto/another foreign currency exchange rates & prices e.g.
+        - GBP (foreign curr.) => RUB (another foreign curr.),
+        - CNY (foreign curr.) => BTC (crypto curr.),
+        - [!] but not: BTC (cryptocurrency) => ETH (another cryptocurrency)
+    - 'crypto_exchange_pair' is for cryptocurrency-to-cryptocurrency exchange rates & prices e.g.
+        - BTC (Bitcoin is cryptocurrency) => ETH (Ethereum is another cryptocurrency),
+        - ETH (Ethereum) => XMR (Monero),
+        - XMR (Monero) => ETH (Ethereum) - reverse order conversion is treated
+        as different pair (because rates and prices may vary)
+    """
     sm_instrument = "stock_market_instrument"
     curr_pair = "currency_exchange_pair"
     crypto_pair = "crypto_exchange_pair"
 
 
 class UserEntity(BaseModel):
-    id: UUID = Field(default_factory=uuid4)
+    """
+    Represents a bot user
+
+    - 'id' is an inner DB table identifier (expected to be unique in the table) [NOT NULL];
+    - 'created_at' is a timestamp (with timezone information) which indicates
+    at what time this user was added to DB (could be automatically assigned at user creation) [NOT NULL];
+    - 'tg_user_id' is a telegram id of this user which uses the bot (unique for each user in telegram) [NOT NULL];
+    - 'settings' is a JSON field which stores user's settings [NULLABLE]
+    """
+    id: UUID4 = Field(default_factory=uuid4)
     created_at: datetime.datetime
     tg_user_id: int
     settings: dict | None
 
 
 class InstrumentEntity(BaseModel):
-    id: UUID = Field(default_factory=uuid4)
+    """
+    Represents a financial instrument, be it a stock, a currency pair, or a cryptocurrency pair
+    """
+    id: UUID4 = Field(default_factory=uuid4)
     updated_at: datetime.datetime
     symbol: str
     figi_code: str | None
@@ -32,7 +58,10 @@ class InstrumentEntity(BaseModel):
 
 
 class TrackingEntity(BaseModel):
-    id: UUID = Field(default_factory=uuid4)
+    """
+    Represents a tracking order of existing financial instrument created by bot user
+    """
+    id: UUID4 = Field(default_factory=uuid4)
     created_at: datetime.datetime
     instrument: UUID = Field(default_factory=uuid4)
     tracked_by: UUID = Field(default_factory=uuid4)
